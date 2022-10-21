6a8a210e8ac92c980a948cd152ce66f2aebb6205
AliJonMadaminov <aliufc2004@gmail.com>
2022-10-21 12:46:47 +0500

    added filter

--- app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,11 +17,11 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcel
 import android.os.Parcelable
 import androidx.lifecycle.LiveData
 import com.example.android.marsrealestate.overview.MarsApiStatus
 import com.squareup.moshi.Json
-import kotlinx.android.parcel.Parcelize
 
 /**
  * Gets Mars real estate property information from the Mars API Retrofit service and updates the
@@ -29,7 +29,7 @@ import kotlinx.android.parcel.Parcelize
  * Deferred, which we await to get the result of the transaction.
  * @param filter the [MarsApiFilter] that is sent as part of the web server request
  */
-@Parcelize
+
 data class MarsProperty(
         val id: String,
         // used to map img_src from the JSON to imgSrcUrl in our class
@@ -38,4 +38,33 @@ data class MarsProperty(
         val price: Double) : Parcelable {
     val isRental
         get() = type == "rent"
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readDouble(),
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(imgSrcUrl)
+        parcel.writeString(type)
+        parcel.writeDouble(price)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MarsProperty> {
+        override fun createFromParcel(parcel: Parcel): MarsProperty {
+            return MarsProperty(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MarsProperty?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
